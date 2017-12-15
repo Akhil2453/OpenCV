6c8989b631169ca498c5ae03dfb494b72cd36954
Akhil <akhil.menon94@gmail.com>
2017-12-15 10:41:09 +0530

    siplay height and width of the image

--- display_size.py
+++ display_size.py
@@ -6,4 +6,7 @@ import numpy as np
 
 img = cv2.imread('input.jpg')
 
-print img.shape
+print img.shape			#prints the height and width of the image in pixels
+
+print "The height of the image is ", int(img.shape[0]), "pixels"
+print "The width of the image is ", int(img.shape[1]), "pixels"
